e8deb5fb8eed33afc364bcb66f5b4c4a7b6c034b
Re-Programmed <reprogrammed.yt@gmail.com>
2024-02-08 19:50:08 -0700

    Added events to the home page.

---
+++ event_display_manager.js
@@ -0,0 +1,41 @@
+class Event
+{
+    Name;
+    DueDate;
+    Text;
+
+    GetElement()
+    {
+        return EventTemplate.replace("%title%", this.Name).replace("%duedate%", this.DueDate).replace("%content%", this.Text);
+    }
+
+    constructor(name, duedate, text)
+    {
+        this.Name = name;
+        this.DueDate = duedate;
+        this.Text = text;
+    }
+}
+
+const events = [
+    new Event("Writers' Digest", "Feb 4", "https://www.writersdigest.com/writers-digest-competitions/annual-writing-competition<br><br>If anyone is curious about submitting to competitions, here's a link to another. \"Early-Bird\" deadline is May 6th, so there's plenty of time!")
+]
+
+function LoadEvents()
+{
+    events.forEach(event => {
+        document.getElementById("upcoming_events").innerHTML += event.GetElement();
+    })
+}
+
+function SetUpcomingPrompt(promptText)
+{
+    document.getElementById("current_prompt").children[1].innerHTML = promptText;
+}
+
+const EventTemplate = `<div class="event_incoming">
+<h1>%title%</h1>
+<em>(%duedate%)</em>
+
+<p>%content%</p>
+</div>`
